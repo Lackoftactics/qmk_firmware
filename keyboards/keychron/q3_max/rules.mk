fb5cfd1fa8845eeff7df96db4b898f7ec64da57e
Przemyslaw Mroczek <przemyslawmroczek@o2.pl>
2026-05-10 21:06:04 +0200

    Fixed so issues from keyboard chatter that I didn't have time before. I don't know why I waited so long for this

--- keyboards/keychron/q3_max/rules.mk
+++ keyboards/keychron/q3_max/rules.mk
@@ -2,9 +2,3 @@ include keyboards/keychron/common/wireless/wireless.mk
 include keyboards/keychron/common/keychron_common.mk
 
 VPATH += $(TOP_DIR)/keyboards/keychron
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE     = yes
-ENCODER_ENABLE   = yes   # knob
-# Optional: shrink size
-LTO_ENABLE       = yes
-# Do NOT set VIA_ENABLE; VIA can’t see tap-dance/combos anyway
